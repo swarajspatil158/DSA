531bbca462225a3c867b80867fa0a1e25361858f
Swaraj Patil <swarajspatil158@gmail.com>
2021-05-28 20:02:57 +0530

    Stack implementation Added

---
+++ Stack/stack.py
@@ -0,0 +1,33 @@
+"""
+Stack Data Structure.
+"""
+class Stack():
+    def __init__(self):
+        self.items= []
+    
+    def push(self,item):
+        self.items.append(item)
+    
+    def pop(self):
+        return self.items.pop()
+    
+    def is_empty(self):
+        return self.items == []
+
+    def get_stack(self):
+        return self.items
+    
+    def peek(self):
+        if not self.is_empty():
+            return self.items[-1]
+
+myStack = Stack()
+myStack.push("A")
+myStack.push("B")
+print(myStack.get_stack())
+myStack.push("C")
+print(myStack.get_stack())
+myStack.pop()
+print(myStack.get_stack())
+print(myStack.is_empty())
+print(myStack.peek() ,"is on top of stack")
